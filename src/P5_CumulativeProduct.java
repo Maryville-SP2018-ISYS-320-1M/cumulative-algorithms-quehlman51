be651314df9e9f9cce3d893ef5f37fe0b4296dd5
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 02:43:13 -0500

    Wrote code

--- src/P5_CumulativeProduct.java
+++ src/P5_CumulativeProduct.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P5_CumulativeProduct {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner( System.in );
+		System.out.print( "How many numbers do you want to multiply? ");
+		int x = input.nextInt();
+	
+		int product = 1;
+		
+		for(int i = 0; i < x; i++) {
+			System.out.print( "Enter number " + (i + 1) + ": " );
+			int n = input.nextInt();
+			product *= n;
+		}
+		
+		input.close();
+		System.out.println("The final product is: " + product);
 		
 	}
 
