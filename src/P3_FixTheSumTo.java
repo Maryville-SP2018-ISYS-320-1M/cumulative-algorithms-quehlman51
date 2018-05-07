f75d162c8b1b6cd976da5995f751545b0eabf0ed
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 02:41:11 -0500

    Input information and adjusted code

--- src/P3_FixTheSumTo.java
+++ src/P3_FixTheSumTo.java
@@ -6,23 +6,23 @@
 
 /*
 3. What problems did you identify in the sumTo method below?
-
-
+	Needed to move the sum integer definition from inside the for loop to outside.
+    Added the return statment for the method
 */
 
 public class P3_FixTheSumTo {
 
 	public static void main(String[] args) {
-		int totalSum = sumTo( 5 );
+		int totalSum = sumTo(5);
 		System.out.println("The total sum is: " + totalSum );
 
 	}
 	
-	public static int sumTo(int n) { 
-	    for (int i = 1; i <= n; i++) {
-	        int sum = 0; 
+	public static int sumTo(int n) {    
+		int sum = 0; 
+	   	for (int i = 1; i <= n; i++) { 
 	        sum += i;
 	    }
-	}
-
+		return sum;
+	}	
 }
