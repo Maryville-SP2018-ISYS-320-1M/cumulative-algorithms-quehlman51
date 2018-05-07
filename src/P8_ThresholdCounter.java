ddb576da6e211c582eb8d000d70ba006471d9cc8
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 02:50:48 -0500

    Created program

--- src/P8_ThresholdCounter.java
+++ src/P8_ThresholdCounter.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
+	Name(s): Anthony Quehl
 	Date: 
 */
 
 public class P8_ThresholdCounter {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner( System.in );
+		System.out.print( "How many numbers are in the series? ");
+		int x = input.nextInt();
+		System.out.print( "What is the threshold? ");
+		int threshold = input.nextInt();
+	
+		int numberAbove = 0;
+		
+		for(int i = 0; i < x; i++) {
+			System.out.print( "Enter number " + (i + 1) + ": " );
+			int n = input.nextInt();
+			
+			if(n > threshold) {
+				numberAbove++;
+			}
+		}
+		
+		input.close();
+		System.out.println("The count of numbers above the threshold is:  " + numberAbove);
 	}
 
 }
